398528f7df2264a65e262b127b6949cc3d8919d8
i32camad <dcmartinez95@hotmail.com>
2019-11-15 12:33:30 +0100

    support for the parser

--- code/train.py
+++ code/train.py
@@ -1,17 +1,12 @@
-import ee
-import time
 import os
 import sys
 import random as rn
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import shutil
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 
-from functions import addNDVI, normalizedDifference, cleanExperimentFolder, defineArgParsers
+from functions import addNDVI, normalizedDifference, cleanExperimentFolder, defineArgParsersTrain
 
 # Fijar semillas para reproducibilidad de resultados (antes de importar Keras)
 seed = 42
@@ -29,39 +24,33 @@ from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint,Te
 
 def trainModel(args):
 
+	# Get the label for each class
+	labels = args.labels.split(',')
+	num_classes = len(labels)
+
 	# dataset path
-	trainFilePath = os.path.join(args.datasets_path,args.trainDataName)
-	testFilePath= os.path.join(args.datasets_path,args.testDataName)
+	parentPath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	trainFilePath = os.path.join(parentPath,args.datasets_path,args.trainDataName)
 
 	# Load the csv tables and get the dataframes
 	trainDataFrame = pd.read_csv(trainFilePath)
 	print('csv for training loaded')
-	testDataFrame = pd.read_csv(testFilePath)
-	print('csv for testing loaded')
 
 	# Get a numpy array of the dataframe
 	train_n = trainDataFrame.to_numpy()
-	test_n = testDataFrame.to_numpy()
-
-	# shuffle only the train dataset
-	np.random.shuffle(train_n)
 
 	x_train = train_n[:,:-1]
 	y_train = train_n[:,-1]
-	x_test = test_n[:,:-1]
-	y_test = test_n[:,-1]
 
 	# Get the x_val and y_val
 	x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2) # 80% train, 20% val
 
 	# convert class vectors to binary class matrices
-	y_train = keras.utils.to_categorical(y_train, args.num_classes)
-	y_val = keras.utils.to_categorical(y_val, args.num_classes)
-	y_test = keras.utils.to_categorical(y_test, args.num_classes)
+	y_train = keras.utils.to_categorical(y_train, num_classes)
+	y_val = keras.utils.to_categorical(y_val, num_classes)
 
 	# Add NDVI
 	x_train = addNDVI(x_train)
-	x_test = addNDVI(x_test)
 	x_val = addNDVI(x_val)
 
 	# Number of features
@@ -72,12 +61,10 @@ def trainModel(args):
 	nLayers = len(args.nNeurons)
 
 	# Experiment folder and name
-	nameExperimentsFolder = 'experiments'
-	nameExperiment = 'land_classification'
 	nameModel = 'lr|%.1e-bs|%d-drop|%.2f-hla|%d-hne|%s-epo|%d' % (args.learning_rate,args.batch_size,
 		args.percentageDropout,nLayers,str(args.nNeurons),args.epochs)
 	fileExtension = '{epoch:02d}-{val_loss:.4f}.hdf5'
-	experimentPath = os.path.join(nameExperimentsFolder,nameExperiment,nameModel)
+	experimentPath = os.path.join(args.experiment_folder,args.experiment_name,nameModel)
 
 	# Create the folder of experiments where we will save the models
 	if not(os.path.exists(experimentPath)):
@@ -100,13 +87,15 @@ def trainModel(args):
 	model = Sequential()
 	model.add(Dense(args.nNeurons[0], activation=tf.nn.relu, input_dim=num_features_input))
 
-	# Check if the user has input more than one hidden layer
+	# Check if the user has, in the input, more than one hidden layer
 	if nLayers > 1:
 		for i in range(1,nLayers):
 			model.add(Dense(args.nNeurons[i], activation=tf.nn.relu))
 
-	model.add(Dropout(args.percentageDropout))
-	model.add(Dense(args.num_classes, activation='softmax'))
+	# Check if the user wants to use a dropout layer
+	if args.percentageDropout > 0.0:
+		model.add(Dropout(args.percentageDropout))
+	model.add(Dense(num_classes, activation='softmax'))
 
 	# Show the neural net
 	print(model.summary())
@@ -132,9 +121,9 @@ def trainModel(args):
 
 def main():
 
-	args = defineArgParsers()
+	args = defineArgParsersTrain()
 
-	#trainModel(args)
+	trainModel(args)
 
 if __name__ == "__main__":
 	main()
