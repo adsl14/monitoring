e7230dbc999c943f92b1f07ee918a21b4c30f80a
i32camad <ferrari_dani_010@hotmail.es>
2020-04-29 17:53:00 +0200

    Reduced the quanty of cases to explore

--- code/train_LSTM+CNN.sh
+++ code/train_LSTM+CNN.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-nameExperiment="rice_t1_balanced"
-sentinels=("A" "B" "A,B" "B,A")
-orbits=("ASC" "DESC" "ASC,DESC" "DESC,ASC")
+nameExperiment="rice_t1_balanced_inversed"
+sentinels=("A" "B" "A,B")
+orbits=("ASC" "DESC" "DESC,ASC")
 indexes_sentinel1=("VH_Sum_VV")
 labels="cumple,no_cumple"
 colors_label="cyan,orange"
-campaings="rice_t1_A,B_DESC,ASC_2017-11-15_2017-12-01|rice_t1_A,B_DESC,ASC_2018-11-15_2018-12-01"
+campaings="rice_t1_A,B_DESC,ASC_2018-11-15_2018-12-01|rice_t1_A,B_DESC,ASC_2017-11-15_2017-12-01"
 tags_name="tags_balanced.csv"
 network="LSTM+CNN"
-percentageGPU=0.225
+percentageGPU=0.14
 
 tam_sentinels=${#sentinels[*]}
 tam_orbits=${#orbits[*]}
 tam_indexes_sentinel1=${#indexes_sentinel1[*]}
 
-lrs=(1e-3 1e-4 1e-5 1e-6)
-batch_sizes=(8 16 32)
-epochs=(100 200)
-dropouts=(0.0 0.2 0.4)
-patiences=(20 40 50)
-patiences_reduce_lr=(5 8 12)
+lrs=(1e-3 1e-4)
+batch_sizes=(16 32)
+epochs=(200)
+dropouts=(0.0 0.4)
+patiences=(40)
+patiences_reduce_lr=(7)
 nNeuronsSequence=("128")
-nNeuronsConv1D=("64,64" "32,32,16,16" "64,64,32,16")
-nNeurons=("64,32,16,8,4" "32,16,8,4" "16,8,4" "8,4" "4" "")
-kernelSize=(1 2 3)
+nNeuronsConv1D=("64,64")
+nNeurons=("128" "")
+kernelSize=(1)
 
 tam_lrs=${#lrs[*]}
 tam_batch_sizes=${#batch_sizes[*]}
@@ -48,7 +48,7 @@ do
 			for ((pr_lr=0; pr_lr<tam_patiences_reduce_lr; ++pr_lr))
 			do
 
-				for ((p=0; pr_lr<tam_patiences; ++p))
+				for ((p=0; p<tam_patiences; ++p))
 				do
 
 					for ((e=0; e<tam_epochs; ++e))
